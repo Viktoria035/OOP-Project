404d1b8496a2cb8f7790c545c3ec8dfb24c5ce21
Viktoria035 <115097521+Viktoria035@users.noreply.github.com>
2023-06-05 21:20:27 +0300

    New functions added

--- User.h
+++ User.h
@@ -2,37 +2,43 @@
 #include <fstream>
 #include "MyString.h"
 #include <iostream>
-
+#include "Address.h"
 class User
 {
-	MyString _userName = "";
-	MyString _password = "";
-	MyString _lastName = "";
-	MyString _firstName = "";
-	MyString _type = "";
+	MyString _type = "Unknown";
+	MyString _userName = "Unknown";
+	MyString _password = "Unknown";
+	MyString _firstName = "Unknown";
+	MyString _lastName = "Unknown";
 
+	size_t coins = 0;
 public:
 	User() = default;
-	User(const MyString& username, const MyString& firstName, const MyString& lastName, const MyString& password, const MyString& type);
+	User(const MyString& type);
+	User(const MyString& type,const MyString& username, const MyString& password, 
+		const MyString& firstName, const MyString& lastName);
+	User(const MyString& username, const MyString& password,
+		const MyString& firstName, const MyString& lastName);
 
+	void setType(const MyString& type);
 	void setUserName(const MyString& name);
 	void setPass(const MyString& pass);
 	void setFirstName(const MyString& first);
 	void setLastName(const MyString& last);
-	void setType(const MyString& type);
+	void setAmount(double amount);
 
+	const MyString& getType() const;
 	const MyString& getUserName() const;
 	const MyString& getPass() const;
 	const MyString& getFirstName() const;
 	const MyString& getLastName() const;
-	const MyString& getType() const;
 
-	void saveInFile(const MyString& filePath);
-	void writeInFile(const MyString& filePath);
+	size_t getCoins() const;
+	double getLeva() const;
+
+	void addMoney(size_t amount);
 
 	friend std::istream& operator>>(std::istream& is, User& user);
 	friend std::ostream& operator<<(std::ostream& os, const User& user);
+	virtual User* clone() const = 0;
 };
-void registration(User& user);
-bool doesUserExists(const MyString& username,const MyString& type);
-//void findUser(User& user);
