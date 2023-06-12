7ef9af4b2493de332cbcce64f19b68310d815393
Viktoria035 <115097521+Viktoria035@users.noreply.github.com>
2023-06-12 18:00:02 +0300

    Cleaning

--- Client.h
+++ Client.h
@@ -4,17 +4,15 @@
 
 class Client :public User
 {
-	//size_t coins = 0;// stotinki in BG
 public:
 	Client();
 	Client(const MyString& username, const MyString& password,
 		const MyString& firstname, const MyString& lastname);
 
-	//void setMoney(double coins);
-	//double getMoney() const;
+	void withdraw(int coins);
 
-	void withdraw(size_t coins);
-	//void addMoney(double leva);
+	void writeClientInFile(std::ofstream& ofs) const;
+	Client readClientFromFile(std::ifstream& ifs);
 
 	friend std::istream& operator>>(std::istream& is, Client& client);
 	friend std::ostream& operator<<(std::ostream& os, const Client& client);
