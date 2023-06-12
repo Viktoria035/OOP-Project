ffd8106bc3ca522d518a04b3c8a3543019e60342
Viktoria035 <115097521+Viktoria035@users.noreply.github.com>
2023-06-12 18:05:11 +0300

    Functions for writing and reading from files

--- Order.cpp
+++ Order.cpp
@@ -1,4 +1,7 @@
 #include "Order.h"
+#include "Utils.h"
+#include <fstream>
+#include <sstream>
 
 int Order::count = 0;
 
@@ -44,17 +47,12 @@ Order::Order(const MyString& origin, int addressX, int addressY, const MyString&
 	count++;
 }
 
-//size_t Order::getId() const
-//{
-//	return orderID;
-//}
-
-//void Order::setAmount(int amount)
-//{
-//	if (amount < 0)
-//		amount = 0;
-//	this->amount = amount;
-//}
+void Order::setAmount(int amount)
+{
+	if (amount < 0)
+		throw std::invalid_argument("Not valid order amount!");
+	this->amount = amount;
+}
 
 void Order::setMinutes(int minutes)
 {
@@ -71,13 +69,17 @@ void Order::setDriverRating(short rate)
 void Order::setStatusByInt(int num)
 {
 	if (num == 0)
-		this->status = StatusOrder::accepted;
-	else if (num == 1)
 		this->status = StatusOrder::inProgress;
+	else if (num == 1)
+		this->status = StatusOrder::accepted;
 	else if (num == 2)
 		this->status = StatusOrder::finished;
 	else if (num == 3)
 		this->status = StatusOrder::canceled;
+	else if (num == 4)
+		this->status = StatusOrder::declined;
+	else if (num == 5)//not sure because this will never happen probably
+		this->status = StatusOrder::paid;
 	else
 		throw std::invalid_argument("Not valid number for order status!");
 }
@@ -90,13 +92,17 @@ void Order::setOrderStatus(const StatusOrder& status)
 void Order::getStatusOfOrder() const
 {
 	if (this->status == StatusOrder::accepted)
-		std::cout << "accepted." << std::endl;
+		std::cout << " accepted." << std::endl;
 	else if (this->status == StatusOrder::canceled)
-		std::cout << "canceled." << std::endl;
+		std::cout << " canceled." << std::endl;
 	else if (this->status == StatusOrder::finished)
-		std::cout << "finished." << std::endl;
+		std::cout << " finished." << std::endl;
 	else if (this->status == StatusOrder::inProgress)
 		std::cout << " in progress." << std::endl;
+	else if (this->status == StatusOrder::declined)
+		std::cout << " declined." << std::endl;
+	else if (this->status == StatusOrder::paid)
+		std::cout << " paid." << std::endl;
 	else
 	{
 		std::cout << "Not valid status!" << std::endl;
@@ -124,7 +130,7 @@ size_t Order::getID() const
 	return orderID;
 }
 
-void Order::check() const
+void Order::checkAllInfo() const
 {
 	std::cout << "The status of the order is: ";
 	getStatusOfOrder();
@@ -134,7 +140,7 @@ void Order::check() const
 	std::cout << "He will be on the address after: " << minutes << " minutes." << std::endl;
 }
 
-void Order::checkInfo() const
+void Order::checkOrderInfo() const
 {
 	std::cout << "Client: " << client->getFirstName() << " " << client->getLastName() << " has made an order from: "
 		<< origin.getName() << ", coordinates: (" << origin.getCoordinates().x << "," << origin.getCoordinates().y << "), note(not neccessery): "
@@ -173,7 +179,7 @@ void Order::setDestination(const Address& address)
 void Order::setPassengersCount(int count)
 {
 	if (count < 0 || count > 6)
-		count = 1;
+		throw std::invalid_argument("Sorry the amount of passengers should be between 1-6!");
 	this->passengersCount = count;
 }
 
@@ -182,7 +188,17 @@ const Client* Order::getClient() const
 	return client;
 }
 
+Client* Order::getClient()
+{
+	return client;
+}
+
 const Driver* Order::getDriver() const
+{
+	return driver;
+}
+
+Driver* Order::getDriver()
 {
 	return driver;
 }
@@ -197,10 +213,10 @@ const Address& Order::getDest() const
 	return destination;
 }
 
-//int Order::getAmount() const
-//{
-//	return amount;
-//}
+int Order::getAmount() const
+{
+	return amount;
+}
 
 int Order::getMinutes() const
 {
@@ -214,27 +230,68 @@ int Order::getPassengersCount() const
 
 void Order::clear()
 {
-	(*driver).setStatus(StatusDriver::Free);
+	driver->setStatus(StatusDriver::Free);
 	status = StatusOrder::canceled;
 }
 
-void Order::giveDriverMoneyFromClient(size_t amount)
+void Order::withdrawMoneyFromClient(size_t amount)
 {
-	(*driver).addMoney(amount);
 	(*client).withdraw(amount);
 }
 
 void Order::setDriverStatus(const StatusDriver& curr)
 {
-	(*driver).setStatus(curr);
+	driver->setStatus(curr);
 }
 
 void Order::setDriverAddress(const Address& ad)
 {
-	(*driver).setAddress(ad);
+	driver->setAddress(ad);
 }
 
-void Order::addDriverAmount(double amount)
+void Order::addDriverAmount(int amount)
 {
-	(*driver).addMoney(amount);
+	driver->addMoney(amount);
+}
+
+void Order::writeOrderInFile(std::ofstream& ofs) const
+{
+	ofs << getOrigin().getName() << ",";
+	ofs << getOrigin().getCoordinates().x << ",";
+	ofs << getOrigin().getCoordinates().y << ",";
+	ofs << getOrigin().getNote() << ",";
+	ofs << getDest().getName() << ",";
+	ofs << getDest().getCoordinates().x << ",";
+	ofs << getDest().getCoordinates().y << ",";
+	ofs << getDest().getNote() << ",";
+	ofs << getPassengersCount() << ",";
+	ofs << getClient()->getUserName() << ",";
+	ofs << getDriver()->getUserName() << ",";
+	ofs << getMinutes() << ",";
+	ofs << (int)getStatus() << ",";
+	ofs << getAmount() << std::endl;
+}
+
+Order Order::readOrderFromFile(std::ifstream& ifs,Vector<Client>& clients, Vector<Driver>& drivers)
+{
+	static char* messegesOrders[] = { (char*)"address",(char*)"coordinates X",(char*)"coordinates Y",(char*)"noteOrigin",(char*)"destination",
+	(char*)"coordinates X",(char*)"coordinates Y",(char*)"noteDest",(char*)"passengers count",(char*)"client username",(char*)"driver username",
+	(char*)"minutes",(char*)"status" ,(char*)"amount" };
+	char buff[sizeof(messegesOrders) / sizeof(char*)][1024];
+	char buff2[BUFF_SIZE];
+	ifs.getline(buff2, 1024);
+	std::stringstream ss(buff2);
+	for (int i = 0; i < sizeof(messegesOrders) / sizeof(char*); i++)
+	{
+		ss.getline(buff[i], 1024, ',');
+	}
+	setAddress(buff[0], buff[3], fromStringToInt(buff[1]), fromStringToInt(buff[2]));
+	setDestination(buff[4], buff[7], fromStringToInt(buff[5]), fromStringToInt(buff[6]));
+	setPassengersCount(fromStringToInt(buff[8]));
+	setClient(findClient(clients,buff[9]));
+	setDriver(findDriver(drivers,buff[10]));
+	setMinutes(fromStringToInt(buff[11]));
+	setStatusByInt(fromStringToInt(buff[12]));
+	setAmount(fromStringToInt(buff[13]));
+	return (*this);
 }
