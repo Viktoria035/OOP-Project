310bf0ca803e8728b0ec1add63815b63b9512bf4
Viktoria035 <115097521+Viktoria035@users.noreply.github.com>
2023-06-18 17:30:47 +0300

    Changes in some types of variables

--- User.cpp
+++ User.cpp
@@ -1,45 +1,112 @@
-#pragma once
-#include <fstream>
-#include <iostream>
-#include "MyString.h"
-#include "Vector.hpp"
+#include "User.h"
+#include "Utils.h"
 
-class User
+User::User(const MyString& type):_type(type) {}
+
+User::User(const MyString& username, const MyString& password,
+	const MyString& firstName, const MyString& lastName)
 {
-	int coins = 0;
+	setUserName(username);
+	setPass(password);
+	setFirstName(firstName);
+	setLastName(lastName);
+}
 
-	MyString _type = "Unknown";
-	MyString _userName = "Unknown";
-	MyString _password = "Unknown";
-	MyString _firstName = "Unknown";
-	MyString _lastName = "Unknown";
+void User::setUserName(const MyString& name)
+{
+	_userName = name;
+}
 
-	Vector<MyString> notifications;
-public:
-	User() = default;
-	User(const MyString& type);
-	User(const MyString& username, const MyString& password,
-		const MyString& firstName, const MyString& lastName);
+void User::setPass(const MyString& pass)
+{
+	_password = pass;
+}
 
-	void setUserName(const MyString& name);
-	void setPass(const MyString& pass);
-	void setFirstName(const MyString& first);
-	void setLastName(const MyString& last);
-	void setAmount(int amount);
+void User::setFirstName(const MyString& first)
+{
+	_firstName = first;
+}
 
-	const MyString& getType() const;
-	const MyString& getUserName() const;
-	const MyString& getPass() const;
-	const MyString& getFirstName() const;
-	const MyString& getLastName() const;
+void User::setLastName(const MyString& last)
+{
+	_lastName = last;
+}
 
-	int getCoins() const;
+void User::setAmount(int amount)
+{
+	if (amount < 0)
+		throw std::invalid_argument("Not valid amount!");
+	this->coins = amount;
+}
 
-	void addMoney(int amount);
-	void addNotification(const MyString& str);
-	void printAllNotifications() const;
+const MyString& User::getType() const
+{
+	return _type;
+}
 
-	friend std::istream& operator>>(std::istream& is, User& user);
-	friend std::ostream& operator<<(std::ostream& os, const User& user);
-	virtual User* clone() const = 0;
-};
+const MyString& User::getUserName() const
+{
+	return _userName;
+}
+
+const MyString& User::getPass() const
+{
+	return _password;
+}
+
+const MyString& User::getFirstName() const
+{
+	return _firstName;
+}
+
+const MyString& User::getLastName() const
+{
+	return _lastName;
+}
+
+int User::getCoins() const
+{
+	return coins;
+}
+
+void User::addMoney(int amount)
+{
+	if (amount < 0)
+		throw std::invalid_argument("Not valid amount!");
+	this->coins += amount;
+}
+
+std::istream& operator>>(std::istream& is, User& user)
+{
+	is >> user._type;
+	is >> user._userName;
+	is >> user._password;
+	is >> user._firstName;
+	is >> user._lastName;
+	return is;
+}
+
+std::ostream& operator<<(std::ostream& os, const User& user)
+{
+	os << "Type: " << user._type << ", Username: " << user._userName << ", Pass: " << user._password
+		<< ", FirstName: " << user._firstName << ", LastName: " << user._lastName;
+	return os;
+}
+
+void User::addNotification(const MyString& str) 
+{
+	notifications.push_back(str);
+}
+
+void User::printAllNotifications() const
+{
+	if (notifications.getSize() == 0)
+		std::cout << "<Empty>" << std::endl;
+	else
+	{
+		for (size_t i = 0; i < notifications.getSize(); i++)
+		{
+			std::cout << notifications[i] << std::endl;
+		}
+	}
+}
